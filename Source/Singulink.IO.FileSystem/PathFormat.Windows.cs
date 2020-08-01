3cd93ee43ea846cba59c5d63f8717a5fc4007d0e
Mike Marynowski <mikem@singulink.com>
2020-08-01 01:28:05 -0400

    Remove unnecessary dependencies.

--- Source/Singulink.IO.FileSystem/PathFormat.Windows.cs
+++ Source/Singulink.IO.FileSystem/PathFormat.Windows.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using Singulink.IO.Utilities;
