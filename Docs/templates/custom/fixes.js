2841e40477eb8499ad3d2a31c23427ea404182f3
Mike Marynowski <mikem@singulink.com>
2021-02-05 13:11:36 -0500

    Update docs and use new template

---
+++ Docs/templates/custom/fixes.js
@@ -0,0 +1,19 @@
+// Remove empty table columns:
+
+$(function() {
+    $('table').each(function(a, tbl) {
+        var currentTableRows = $(tbl).find('tbody tr').length;
+        $(tbl).find('th').each(function(i) {
+            var remove = 0;
+            var currentTable = $(this).parents('table');
+
+            var tds = currentTable.find('tr td:nth-child(' + (i + 1) + ')');
+            tds.each(function(j) { if ($(this).text().trim() === '') remove++; });
+
+            if (remove == currentTableRows) {
+                $(this).hide();
+                tds.hide();
+            }
+        });
+    });
+});
